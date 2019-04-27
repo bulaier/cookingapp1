a67e1adab011c849cd343090c453ffb10b732adb
yizhenm2 <yizhenm2@illinois.edu>
2019-04-27 15:25:57 -0500

    gson implementation

--- lib/src/main/java/com/example/lib/Recipe.java
+++ lib/src/main/java/com/example/lib/Recipe.java
@@ -1,8 +1,14 @@
 package com.example.lib;
+import  com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 public class Recipe {
     private int min;
     private String[] ingredients;
+    private int id;
+    private String title;
     public String getIngredients() {
         String str = "";
         for (int i = 0; i < ingredients.length; i++) {
