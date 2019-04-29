89f437ac9d4a2a34e70583e746d63d8d987dd59d
yizhenm2 <yizhenm2@illinois.edu>
2019-04-29 00:06:40 -0500

    The search engine finished.

--- lib/src/main/java/com/example/lib/Recipe.java
+++ lib/src/main/java/com/example/lib/Recipe.java
@@ -1,21 +1,21 @@
 package com.example.lib;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Recipe {
-    private String[] ingredients;
+    private ArrayList<String> ingredients;
     private String instruction;
     private String title;
     private URL image;
 
 
     // the method to return ingredients. all small cases, no plurals.
-    public String getIngredients() {
-        String str = "";
-        for (int i = 0; i < ingredients.length; i++) {
-            str += ingredients[i];
-        }
-        return str;
+    public ArrayList<String> getIngredients() {
+        ArrayList<String> str = new ArrayList<>();
+
+        return ingredients;
     }
     //return instructions as a string. Including the amount of each ingredients.
     public String getInstruction() {
@@ -25,9 +25,13 @@ public final class Recipe {
     public void setInstruction(String s) {
         instruction = s;
     }
-    public void setIngredients(String ingre) {
+    //it is a setter of the ingredients, but also returns for convenience.
+    public ArrayList<String> setIngredients(String ingre) {
         String[] str = ingre.split(", ");
-        ingredients = str;
+        for (int i = 0; i < str.length; i++) {
+            ingredients.add(str[i]);
+        }
+        return ingredients;
     }
     public void setImage(String s) throws Exception {
         image = new URL(s);
---
+++ lib/src/main/java/com/example/lib/Search.java
@@ -0,0 +1,32 @@
+package com.example.lib;
+
+import java.util.ArrayList;
+
+public class Search {
+    private static int number = Testing.store.length;
+
+    public static ArrayList<Recipe> search(String str) {
+        if (str == null) {
+            return null;
+        }
+        ArrayList<String> ingredients = new ArrayList<>();
+        ArrayList<Recipe> recipes = new ArrayList<>();
+        String[] s = str.split(", ");
+        for (int i = 0; i < s.length; i++) {
+            ingredients.add(s[i]);
+        }
+        for (int i = 0; i < number; i++) {
+            boolean b = true;
+            for (String name : Testing.store[i].getIngredients()) {
+                if (!ingredients.contains(name)) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) {
+                recipes.add(Testing.store[i]);
+            }
+        }
+        return recipes;
+    }
+}
