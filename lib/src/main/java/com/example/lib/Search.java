23544480bf21b3c95522c6b3a866cdf468359414
yizhenm2 <yizhenm2@illinois.edu>
2019-04-29 00:19:14 -0500

    Library refined. (I'll add recipes later.)

--- lib/src/main/java/com/example/lib/Search.java
+++ lib/src/main/java/com/example/lib/Search.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Search {
     private static int number = Testing.store.length;
 
+    // seperate the ingredients with " ," and all small case letters. No plurals please.
     public static ArrayList<Recipe> search(String str) {
         if (str == null) {
             return null;
